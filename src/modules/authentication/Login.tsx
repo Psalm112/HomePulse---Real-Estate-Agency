e4c58592020545ebd7f68596b01dc853ca4d11f7
Samuel Oyenuga <psa8.sa@gmail.com>
2024-11-09 20:19:01 +0100

    Update Login.tsx

--- src/modules/authentication/Login.tsx
+++ src/modules/authentication/Login.tsx
@@ -18,9 +18,9 @@ function Login() {
             <h1 className="mohave text-offwhite font-medium text-3xl mb-[2vh] relative after:content-[''] after:absolute after:bottom-[-5px] after:right-[-5px] after:block after:h-[50%] after:w-[30%] after:border-[#00bbba] after:border-dotted after:border-b-[1px] after:border-r-[1px] after:animate-[fillBorder_1s_linear] before:content-[''] before:absolute before:top-[-5px] before:left-[-5px] before:block before:h-[50%] before:w-[30%] before:border-[#00bbba] before:border-dotted before:border-t-[1px] before:border-l-[1px] before:animate-[fillBorder_1s_linear] md:mb-[10vh] md:text-5xl">
               HOME PJLSE
             </h1>
-            <p className="max-w-[80%] text-offwhite font-light text-base md:hidden">
+{/*             <p className="max-w-[80%] text-offwhite font-light text-base md:hidden">
               Capture your personal memories in a unique way, anywhere.
-            </p>
+            </p> */}
             {!forgotPassword ? (
               <LoginForm
                 setIsOtp={setIsOtp}
